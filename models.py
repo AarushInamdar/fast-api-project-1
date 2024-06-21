0cc776647e7e3fc13e0edd340b96a256ef032075
Aarush Inamdar <128437601+AarushInamdar@users.noreply.github.com>
2024-06-20 23:03:43 -0700

    SQL added and Class user created

--- models.py
+++ models.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base
 
 class Todo(BaseModel):
     id: int
     item: str
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True)
+    
